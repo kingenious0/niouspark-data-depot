ef526179f85fdc511d70c69ecd3ae11cd3bcbbd0
kingenious0 <kingenious0@gmail.com>
2025-08-20 05:33:30 +0000

    Update next.config.ts

--- vercel/path0/next.config.ts
+++ vercel/path0/next.config.ts
@@ -1,25 +1,54 @@
-import type { NextConfig } from "next";
+import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  typescript: { ignoreBuildErrors: true },
-  eslint: { ignoreDuringBuilds: true },
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     remotePatterns: [
-      { protocol: "https", hostname: "placehold.co", pathname: "/**" },
-      { protocol: "https", hostname: "images.unsplash.com", pathname: "/**" },
-      { protocol: "https", hostname: "play-lh.googleusercontent.com", pathname: "/**" },
-      { protocol: "https", hostname: "upload.wikimedia.org", pathname: "/**" },
-      { protocol: "https", hostname: "encrypted-tbn0.gstatic.com", pathname: "/**" },
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'play-lh.googleusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'upload.wikimedia.org',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'encrypted-tbn0.gstatic.com',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
-  serverExternalPackages: [
-    "google-auth-library",
-    "farmhash-modern",
-    "firebase-admin",
-  ],
+  experimental: {
+    serverComponentsExternalPackages: ['@genkit-ai/google-cloud']
+  },
+  serverExternalPackages: ['google-auth-library', 'firebase-admin', 'farmhash-modern'],
   env: {
     NEXT_PUBLIC_SUPER_ADMIN_EMAIL: process.env.SUPER_ADMIN_EMAIL,
-  },
+  }
 };
 
 export default nextConfig;
